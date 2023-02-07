8252ebf8c930f98c47fc38922d6090b02f8ad2ee
Muqiu Han <muqiu-han@outlook.com>
2023-02-07 14:16:36 +0800

    Add callstack and backtraces info

--- lib/Omtl.ml
+++ lib/Omtl.ml
@@ -11,12 +11,26 @@ let ( >== ) (name : string) (f : 'a) : 'a test_case = name, f
 (** Wrapper function to failwith *)
 let fail = failwith
 
-(** Print detailed error message *)
-let backtraces (backtraces : Printexc.raw_backtrace) : string =
-  Printexc.raw_backtrace_to_string backtraces
-  |> String.split_on_char '\n'
-  |> List.map (( ^ ) "\t")
-  |> String.concat "\n"
+let backtraces () : string =
+  let backtraces : string =
+    Printexc.get_raw_backtrace ()
+    |> Printexc.raw_backtrace_to_string
+    |> String.split_on_char '\n'
+    |> List.map (( ^ ) "\t")
+    |> String.concat "\n"
+  in
+  Format.sprintf "%s" backtraces
+;;
+
+let callstack () : string =
+  let callstack : string =
+    Printexc.get_callstack 20
+    |> Printexc.raw_backtrace_to_string
+    |> String.split_on_char '\n'
+    |> List.map (( ^ ) "\t")
+    |> String.concat "\n"
+  in
+  Format.sprintf "%s" callstack
 ;;
 
 (** Returns the time the [f] took to run and the [f] execution result.
@@ -29,7 +43,7 @@ let time (f : 'a) : float * 'b =
     Unix.gettimeofday () -. timer, Ok result
   with
   | Failure s -> timer, Fail s
-  | _ -> fail ""
+  | _ -> timer, Fail (callstack ())
 ;;
 
 let test_case (test_case : 'a test_case) : string =
@@ -44,7 +58,7 @@ let test_case (test_case : 'a test_case) : string =
   | Fail s ->
     Format.sprintf
       "   \027[31mo\027[0m- %s...\027[31mFAIL\027[0m \027[37m(0s)\027[0m\n\
-      \        \027[31m!!![Error Info]: %s\027[0m"
+      \        \027[31m|!| %s\027[0m"
       name
       s
 ;;
