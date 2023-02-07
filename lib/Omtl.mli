923764d539d19e1f594e5e9a5456c14631c32c92
Muqiu Han <muqiu-han@outlook.com>
2023-02-07 19:06:47 +0800

    Regenerate the interface file

--- lib/Omtl.mli
+++ lib/Omtl.mli
@@ -1,39 +1,8 @@
-(**********************************************************************************)
-(* MIT License                                                                    *)
-(*                                                                                *)
-(* Copyright (c) 2023 Muqiu Han                                                   *)
-(*                                                                                *)
-(* Permission is hereby granted, free of charge, to any person obtaining a copy   *)
-(* of this software and associated documentation files (the "Software"), to deal  *)
-(* in the Software without restriction, including without limitation the rights   *)
-(* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      *)
-(* copies of the Software, and to permit persons to whom the Software is          *)
-(* furnished to do so, subject to the following conditions:                       *)
-(*                                                                                *)
-(* The above copyright notice and this permission notice shall be included in all *)
-(* copies or substantial portions of the Software.                                *)
-(*                                                                                *)
-(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     *)
-(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       *)
-(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    *)
-(* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         *)
-(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  *)
-(* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  *)
-(* SOFTWARE.                                                                      *)
-(**********************************************************************************)
-
-type 'a test_result =
-  | Ok of 'a
-  | Fail of info * backtraces * callstack
-
-and info = string
-and backtraces = string
-and callstack = string
-
-type 'a test_suit = callstack * 'a test_case list
-and 'a test_case = callstack * 'a
-
+type 'a test_suit = string * 'a test_case list
+and 'a test_case = string * 'a
 val ( +:> ) : string -> 'a test_case list -> string * 'a test_case list
 val ( >== ) : string -> 'a -> 'a test_case
 val fail : string -> 'a
-val run : (unit -> 'a) test_suit -> unit
+val test_case : (unit -> unit) test_case -> string
+val test_suit : (unit -> unit) test_suit -> unit
+val run : (unit -> unit) test_suit -> unit
